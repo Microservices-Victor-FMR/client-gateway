194182309cc65fe7863d791bc5802c9eb305e9d2
victor martinez <vitico841@gmail.com>
2025-02-11 10:10:29 -0400

    Agregar soporte para la variable de entorno NODE_ENV utilizando cross-env y mejorar los mensajes de registro en el arranque de la aplicación.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {RpcCustomExceptionFilter } from './common/exeption/rpc-exception.filter';
+import { RpcCustomExceptionFilter } from './common/exeption/rpc-exception.filter';
 
 async function bootstrap() {
-  const logger= new Logger('Main',{timestamp: true});
-  const app = await NestFactory.create(AppModule,);
+  const ENV = process.env.NODE_ENV || 'development';
+  const consoleLogger = new Logger('Main', { timestamp: true });
+
+  consoleLogger.log(`El Gateway está corriendo en modo ${ENV}`);
+
+  const app = await NestFactory.create(AppModule);
+  app.useLogger(ENV === 'development' ? ['debug','verbose','error','warn','log']: ['log','warn','error'])
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   app.useGlobalPipes(
@@ -14,14 +19,13 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-      
     }),
   );
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 15000);
+  consoleLogger.log(`CLIENT GATEWAY is running on port ${port}`);
   await app.listen(port);
-  logger.log(`CLIENT GATEWAY is running on port ${port}`);
 }
+
 bootstrap();
-
-
