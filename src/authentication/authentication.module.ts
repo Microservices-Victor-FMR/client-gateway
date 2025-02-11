affbaf39654fab1e8154bb9ccacb55d4b0bad31c
victor martinez <vitico841@gmail.com>
2025-02-11 10:15:02 -0400

    Agregar JwtStrategy como proveedor en el módulo de autenticación

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { NatsModule } from 'src/transports/nats.module';
+import { JwtStrategy } from 'src/security/jwt.strategy';
 
 @Module({
   imports: [NatsModule],
   controllers: [AuthenticationController],
-  providers: [],
+  providers: [JwtStrategy],
 })
 export class AuthenticationModule {}
