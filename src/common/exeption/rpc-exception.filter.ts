78654d5afedd6bb482944098dacafdbaf4d2526d
victor martinez <vitico841@gmail.com>
2025-02-17 09:48:49 -0400

    Mejorar el filtro de excepciones RPC para manejar errores de forma más robusta y devolver respuestas adecuadas.

--- src/common/exeption/rpc-exception.filter.ts
+++ src/common/exeption/rpc-exception.filter.ts
@@ -1,58 +1,60 @@
 import {ArgumentsHost,Catch,ExceptionFilter,HttpException, HttpStatus} from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 
-@Catch(RpcException, HttpException) 
+@Catch(RpcException) 
 export class RpcCustomExceptionFilter implements ExceptionFilter {
   catch(exception: RpcException, host: ArgumentsHost) {
 
             const context = host.switchToHttp();
-            const rpcError = exception.getError();
+            let rpcError = exception.getError()
             const response = context.getResponse();
 
-            if(exception instanceof HttpException){
-              const status = exception.getStatus();
-              const message = exception.getResponse();
-              return response.status(status).json({
-                status: status,
-                message: message
-              })
+            
+            if (!rpcError || typeof rpcError !== 'object') {
+              response.status(500).json({
+                message: 'Error inesperado por parte del servidor',
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+              });
+              return;
             }
-
-
-          if (!rpcError || typeof rpcError !== 'object') {
-            response.status(500).json({
-              message: 'Error inesperado por parte del servidor',
-              status: HttpStatus.INTERNAL_SERVER_ERROR,
-            });
-            return;
-          }
           
 
             const hasRequiredProperties ='statusCode'in rpcError || 'message' in rpcError  || 'status' in rpcError
 
-          if(!hasRequiredProperties){
-            response.status(500).json({
-              message: 'Error inesperado por parte del servidor',
-              status : HttpStatus.INTERNAL_SERVER_ERROR
-            })
-            return
-          }
-
-
-            const status = rpcError['statusCode'] || rpcError['status'] || HttpStatus.INTERNAL_SERVER_ERROR
-            const message = rpcError['message']   || "Internal Server Error"
-            
-          if(typeof status!=='number'){
-            response.status(500).json({
+            if(!hasRequiredProperties){
+              response.status(500).json({
                 message: 'Error inesperado por parte del servidor',
-                status: HttpStatus.INTERNAL_SERVER_ERROR
-            })
-            return
+                status : HttpStatus.INTERNAL_SERVER_ERROR
+              })
+              return
+            }
+
+
+            if ('error' in rpcError && typeof rpcError.error === 'object') {
+               rpcError = rpcError.error;
+               const status = rpcError['statusCode'] || rpcError['status'] || HttpStatus.INTERNAL_SERVER_ERROR
+               const message = rpcError['message']
+              return response.status(status).json({
+                message: message,
+                status: status
+               })
+            }
+
+            
+            const status = rpcError['status'] || rpcError['statusCode'] || HttpStatus.INTERNAL_SERVER_ERROR
+            const message = rpcError['message'] || HttpStatus.INTERNAL_SERVER_ERROR
+
+            if(typeof status !== 'number' ){
+               return response.status(500).json({
+                message: 'Error inesperado por parte del servidor',
+                status : HttpStatus.INTERNAL_SERVER_ERROR
+              })
             }
 
             response.status(status).json({
-            status,
-            message
+            statusCode: status,
+            message: message
             })
   }
 }
+
