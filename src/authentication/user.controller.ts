fda1bfff57d27f03a9ce938d1d1100b9a7f8ed50
victor martinez <vitico841@gmail.com>
2025-03-04 21:32:42 -0400

    Agregar método para actualizar el perfil de usuario y endpoint para validar el token de cambio de correo electrónico en UserController

--- src/authentication/user.controller.ts
+++ src/authentication/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Post, Patch, Body } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { FindByIdDto } from './dto/findById-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('profile')
 export class UserController {
@@ -21,6 +22,26 @@ export class UserController {
   async getProfileId(@Param() id: FindByIdDto) {
     try {
       const result = await firstValueFrom(this.nats_profile.send('findOneUser', id));
+      return result;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  async updateProfile(@Param() id: FindByIdDto, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      const result = await firstValueFrom(this.nats_profile.send('updateUser', { id, updateUserDto }));
+      return result;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get('changed-email/:token')
+  async changedEmailToken(@Param('token') token: string) {
+    try {
+      const result = await firstValueFrom(this.nats_profile.send('changedEmailToken', { token }));
       return result;
     } catch (error) {
       throw new RpcException(error);
